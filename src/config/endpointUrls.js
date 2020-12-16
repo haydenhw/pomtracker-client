43a9b058cf24ff04c7c9be1f0d6c1a6827873f26
hayden7913 <hayden321@gmail.com>
2020-12-15 17:07:12 -0800

    adds netlify config

--- src/config/endpointUrls.js
+++ src/config/endpointUrls.js
@@ -1,4 +1,4 @@
-export const localServerUrl = process.env.REACT_APP_LOCAL_URL || 'http://localhost:5001'
+export const localServerUrl = process.env.REACT_APP_LOCAL_URL || 'http://localhost:5001';
 const cloudServerUrl = process.env.REACT_APP_CLOUD_URL || 'https://lula.wtf/ttflask';
 const baseUrl = process.env.NODE_ENV === 'production' ? cloudServerUrl : localServerUrl;
 export const projectsUrl = `${baseUrl}/api/projects`;
